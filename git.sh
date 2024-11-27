a510915bfa3cc11ca4150d4f89f3e6a36e1de348
Newton <123421090+SKNYYYYYYYY@users.noreply.github.com>
2024-11-27 15:16:52 +0300

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,2 +1,1 @@
-#!/usr/bin/bash
-wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elastic-keyring.gpg
+sudo apt-get install apt-transport-https
