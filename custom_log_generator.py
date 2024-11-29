69aba87be73fbefb59918331f413ed5e448d9e36
Newton <123421090+SKNYYYYYYYY@users.noreply.github.com>
2024-11-29 09:27:18 +0300

    Update custom_log_generator.py

--- custom_log_generator.py
+++ custom_log_generator.py
@@ -1,1 +1,2 @@
-input {  file {    path => "/tmp/custom.log"    start_position => "beginning"    sincedb_path => "/dev/null"}} filter {  grok {    match => { "message" => "%{TIMESTAMP_ISO8601:timestamp} \| %{LOGLEVEL:log_level} \| %{GREEDYDATA:log_message}" }  }output {  elasticsearch {    hosts => ["http://172.19.98.14:9200"]    user => "elastic"      password => ""   }
+input {  file {    path => "/tmp/custom.log"    start_position => "beginning"    sincedb_path => "/dev/null"}} filter {  grok {    match => { "message" => "%{TIMESTAMP_ISO8601:timestamp} \| %{LOGLEVEL:log_level} \| %{GREEDYDATA:log_message}" }  }output {  elasticsearch {    hosts => ["http://172.19.98.14:9200"]    user => "elastic"      password => "Q_qWG7r*kYTz+DPenG9j
+"}
